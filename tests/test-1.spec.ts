298df65abf3ac64250b13c0be524681df67df757
Ayesha-Jabeen1 <126652798+Ayesha-Jabeen1@users.noreply.github.com>
2024-08-22 15:45:10 +0530

    Create test-1.spec.ts

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.amazon.in/');
+  await page.getByRole('link', { name: 'Mobiles' }).click();
+  await page.getByPlaceholder('Search Amazon.in').fill('apple iphone15');
+  await page.getByRole('button', { name: 'Go' }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Apple iPhone 15 (128 GB) - Black' }).nth(1).click();
+  const page1 = await page1Promise;
+  await page1.getByRole('button', { name: 'Add to Cart' }).click();
+});
